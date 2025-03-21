b55dfccb19e9358b8f7a05da00364fbda04490e8
Miguel5018 <miguelgerardosalvadorperez@gmail.com>
2025-03-21 08:31:58 -0600

    Termino de la leccion 16

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -2,15 +2,8 @@ import sqlalchemy as sa
 from flask_babel import _
 from flask_babel import lazy_gettext as _l
 from flask_wtf import FlaskForm
-from wtforms import (
-    BooleanField,
-    PasswordField,
-    StringField,
-    SubmitField,
-    TextAreaField,
-    ValidationError,
-)
-from wtforms.validators import DataRequired, Email, EqualTo, Length
+from wtforms import BooleanField, PasswordField, StringField, SubmitField
+from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
 
 from app import db
 from app.models import User
@@ -41,34 +34,6 @@ class RegistrationForm(FlaskForm):
         user = db.session.scalar(sa.select(User).where(User.email == email.data))
         if user is not None:
             raise ValidationError(_("Please use a different email address."))
-
-
-class EditProfileForm(FlaskForm):
-    username = StringField(_l("Username"), validators=[DataRequired()])
-    about_me = TextAreaField(_l("About me"), validators=[Length(min=0, max=140)])
-    submit = SubmitField(_l("Submit"))
-
-    def __init__(self, original_username, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.original_username = original_username
-
-    def validate_username(self, username):
-        if username.data != self.original_username:
-            user = db.session.scalar(
-                sa.select(User).where(User.username == username.data)
-            )
-            if user is not None:
-                raise ValidationError(_("Please use a different username."))
-
-
-class EmptyForm(FlaskForm):
-    submit = SubmitField("Submit")
-
-
-class PostForm(FlaskForm):
-    post = TextAreaField(_l('Say something'), validators=[DataRequired(), Length(min=1, max=140)]
-    )
-    submit = SubmitField(_l('Submit'))
 
 
 class ResetPasswordRequestForm(FlaskForm):
--- confing.py
+++ confing.py
@@ -1,24 +1,23 @@
 import os
 
+from dotenv import load_dotenv
+
 basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, ".env"))
 
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY") or "you-will-never-guess"
-
     SQLALCHEMY_DATABASE_URI = os.environ.get(
         "DATABASE_URL"
-    ) or "sqlite:///" + os.path.join(basedir, "app.bd")
+    ) or "sqlite:///" + os.path.join(basedir, "app.db")
     MAIL_SERVER = os.environ.get("MAIL_SERVER")
     MAIL_PORT = int(os.environ.get("MAIL_PORT") or 25)
     MAIL_USE_TLS = os.environ.get("MAIL_USE_TLS") is not None
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
-    ADMINS = ["gerardosalvador3434@gmail.com"]
-
+    ADMINS = ["your-email@example.com"]
+    LANGUAGES = ["en"]
     POSTS_PER_PAGE = 25
-    LANGUAGES = ["en", "es"]
-
-    MS_TRANSLATOR_KEY = 'BQ9a3jeqozmTa1hzmhOpjpdDJ0LxYlbuyTICJn3col6zVKvIcF1sJQQJ99BCACYeBjFXJ3w3AAAbACOGdIwt'
-
-    # zllm rkgm nowa topx
+    MS_TRANSLATOR_KEY = "BQ9a3jeqozmTa1hzmhOpjpdDJ0LxYlbuyTICJn3col6zVKvIcF1sJQQJ99BCACYeBjFXJ3w3AAAbACOGdIwt"
+    ELASTICSEARCH_URL = "http://localhost:9200"
